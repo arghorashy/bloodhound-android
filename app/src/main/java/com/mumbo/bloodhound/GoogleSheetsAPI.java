bf09b44fc5b7185e048e333c1d9b897b349d5df6
Afsheen Ghorashy <arghorashy@gmail.com>
2024-03-10 00:06:56 -0500

    ADd different environments for test and prod.

--- app/src/main/java/com/mumbo/bloodhound/GoogleSheetsAPI.java
+++ app/src/main/java/com/mumbo/bloodhound/GoogleSheetsAPI.java
@@ -1,5 +1,8 @@
 package com.mumbo.bloodhound;
 
+import static com.mumbo.bloodhound.BloodhoundEnv.KRISTA;
+import static com.mumbo.bloodhound.BloodhoundEnv.TEST;
+
 import android.app.Activity;
 import android.content.Context;
 import android.util.Log;
@@ -17,11 +20,13 @@ import java.util.concurrent.CompletableFuture;
 
 public class GoogleSheetsAPI {
     // See backend function definition here: https://script.google.com/home/projects/1utIOA5lWcz53UV1_rWB4iHGr12BhR2KfeJPvsQl5Nhr5XsHC8TZeYWs9/arg1?id=45&arg2=Afsheen&arg3=Ghorashy
-    private static final String API_URL = "https://script.google.com/macros/s/AKfycbxEnBZCkS6Xl2IdoJlFrD6jaqzVGb5Ad9EqZ6F19yoBHlO18UIhxnH4FKAM5dmWMQR2/exec";
+    private static final String API_URL = "https://script.google.com/macros/s/AKfycbxS-s4k1_taX7fPF0HVre-G0hwhU3GdY1f80H5RYTo3uW0-NZx_i8nohZpXiFxs1Kgd/exec";
+
+    private static final BloodhoundEnv ENV = KRISTA;
 
     public static void writeRowToLogSheet(Context context, List<String> rowStrings) {
         Gson gson = new Gson();
-        String fullURL = API_URL + "?action=writeRowToLogSheet&row=" + gson.toJson(rowStrings);
+        String fullURL = GoogleSheetsAPI.buildURL("?action=writeRowToLogSheet&row=" + gson.toJson(rowStrings));
         StringRequest req = new StringRequest(
                 Request.Method.GET,
                 fullURL,
@@ -32,7 +37,7 @@ public class GoogleSheetsAPI {
     }
 
     public static CompletableFuture<ArrayList<BloodhoundConfigRow>> readFromConfigSheet(Context context) {
-        String fullURL = API_URL + "?action=readConfig";
+        String fullURL = GoogleSheetsAPI.buildURL("?action=readConfig");
         CompletableFuture<ArrayList<BloodhoundConfigRow>> res = new CompletableFuture<>();
 
         StringRequest req = new StringRequest(
@@ -46,6 +51,21 @@ public class GoogleSheetsAPI {
         RequestQueue q = Volley.newRequestQueue(context);
         q.add(req);
         return res;
+    }
+
+    private static String buildURL(String queryParams) {
+        String envQueryParam;
+        switch (ENV) {
+            case TEST:
+                envQueryParam = "test";
+                break;
+            case KRISTA:
+                envQueryParam = "krista";
+                break;
+            default:
+                throw new IllegalArgumentException("Env not set.");
+        }
+        return API_URL + queryParams + "&env=" + envQueryParam;
     }
 
 }
